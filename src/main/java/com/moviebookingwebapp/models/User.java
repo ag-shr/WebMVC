fd52543e5159bd26593de055256e2aa5cef60831
Shreesh Agrawal <shreesh1234@gmail.com>
2020-11-09 14:54:14 +0530

    Login and Signup functionality

---
+++ src/main/java/com/moviebookingwebapp/models/User.java
@@ -0,0 +1,37 @@
+package com.moviebookingwebapp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	@NotNull(message = "Email can not be null")
+	@Email(message = "Email should be in correct format")
+	private String email;
+	@NotNull(message = "Password can not be null")
+	private String password;
+	@NotNull(message = "Name can not be null")
+	private String name;
+//	@NotNull(message = "Mobile number can not be null")
+//	@Size(min = 10, max = 10)
+//	private String mobile;
+//	private String profilePicture;
+//	@NotNull(message = "First name can not be null")
+//	private String firstName;
+//	@NotNull(message = "Last name can not be null")
+//	private String lastName;
+//	@NotNull(message = "DOB can not be null")
+//	private String dob;
+//	@NotNull(message = "Gender can not be null")
+//	private String gender;
+//	@NotNull(message = "Role can not be null")
+//	private String role;
+}
