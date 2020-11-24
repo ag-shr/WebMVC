da896fbad3e25cd3766b15e9280db7366c4e7ff5
Vinay Pratap Singh <jadonvinay8@gmail.com>
2020-11-24 21:23:08 +0530

    Modified Exception Handler

--- src/main/java/com/webapp/exception/MovieBookingWebAppException.java
+++ src/main/java/com/webapp/exception/MovieBookingWebAppException.java
@@ -3,12 +3,8 @@ package com.webapp.exception;
 import org.springframework.http.HttpStatus;
 
 public class MovieBookingWebAppException extends RuntimeException {
-	private static final long serialVersionUID = -6335764236468722888L;
-	private HttpStatus httpStatus;
 
-	public MovieBookingWebAppException() {
-		super();
-	}
+	private HttpStatus httpStatus;
 
 	public MovieBookingWebAppException(MovieBookingWebAppException e, HttpStatus httpStatus) {
 		super(e);
--- src/main/java/com/webapp/exception/MovieBookingWebAppExceptionHandler.java
+++ src/main/java/com/webapp/exception/MovieBookingWebAppExceptionHandler.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import javax.validation.ConstraintViolationException;
 
 import com.webapp.RequestResponseClasses.ErrorResponse;
-import com.webapp.RequestResponseClasses.MicroserviceResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,70 +17,52 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @ControllerAdvice
 public class MovieBookingWebAppExceptionHandler {
 
-	@ExceptionHandler(value = { MovieBookingWebAppException.class })
-	public ResponseEntity<ErrorMessage> handleTheaterMicroserviceException(MovieBookingWebAppException ex) {
-		String message = ex.getLocalizedMessage();
-		if (message == null)
-			message = "";
-		return new ResponseEntity<ErrorMessage>(new ErrorMessage(new Date(), message, ex.getHttpStatus()),
-				ex.getHttpStatus());
-	}
+    @ExceptionHandler(MovieBookingWebAppException.class)
+    public ResponseEntity<ErrorMessage> handleTheaterMicroserviceException(MovieBookingWebAppException ex) {
+        String message = ex.getLocalizedMessage();
+        if (message == null)
+            message = "";
+        return new ResponseEntity<>(new ErrorMessage(new Date(), message, ex.getHttpStatus()),
+          ex.getHttpStatus());
+    }
 
-	@ExceptionHandler(value = { InterServiceCallException.class })
-	public ResponseEntity<ErrorMessage> handleInterServiceCallException(InterServiceCallException ex) {
-		String message = ex.getLocalizedMessage();
-		if (message == null)
-			message = "";
+
+    @ExceptionHandler(RestClientResponseException.class)
+    public ResponseEntity<ErrorMessage> handleRestClientResponseException(RestClientResponseException ex) {
+        String message = ex.getResponseBodyAsString();
 		ObjectMapper objectMapper = new ObjectMapper();
-		ErrorMessage errorMessage;
-		try {
-			errorMessage = objectMapper.readValue(message, ErrorMessage.class);
-			return new ResponseEntity<ErrorMessage>(errorMessage, errorMessage.getHttpStatus());
-		} catch (JsonProcessingException e) {
-			return new ResponseEntity<ErrorMessage>(new ErrorMessage(new Date(),
-					"Problem occurred while converting json string to object", HttpStatus.INTERNAL_SERVER_ERROR),
-					HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+        ErrorMessage errorMessage;
+        try {
+            errorMessage = objectMapper.readValue(message, ErrorMessage.class);
+            return new ResponseEntity<>(errorMessage, errorMessage.getHttpStatus());
+        } catch (JsonProcessingException e) {
+            return new ResponseEntity<>(new ErrorMessage(new Date(),
+              "Problem occurred while converting json string to object", HttpStatus.INTERNAL_SERVER_ERROR),
+              HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
-	@ExceptionHandler(value = { RestClientResponseException.class })
-	public ResponseEntity<ErrorMessage> handleRestClientResponseException(RestClientResponseException ex) {
-		String message = ex.getResponseBodyAsString();
-		if (message == null)
-			message = "";
-		ObjectMapper objectMapper = new ObjectMapper();
-		ErrorMessage errorMessage;
-		try {
-			errorMessage = objectMapper.readValue(message, ErrorMessage.class);
-			return new ResponseEntity<ErrorMessage>(errorMessage, errorMessage.getHttpStatus());
-		} catch (JsonProcessingException e) {
-			return new ResponseEntity<ErrorMessage>(new ErrorMessage(new Date(),
-					"Problem occurred while converting json string to object", HttpStatus.INTERNAL_SERVER_ERROR),
-					HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorMessage> handleConstraintViolationException(ConstraintViolationException e) {
+        String message = e.getLocalizedMessage();
+        if (message == null)
+            message = e.toString();
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), message, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
 
-	@ExceptionHandler(value = { ConstraintViolationException.class })
-	public ResponseEntity<ErrorMessage> handleConstraintViolationException(ConstraintViolationException e) {
-		String message = e.getLocalizedMessage();
-		if (message == null)
-			message = e.toString();
-		ErrorMessage errorMessage = new ErrorMessage(new Date(), message, HttpStatus.BAD_REQUEST);
-		return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
-	}
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> handleAnyException(Exception e) {
+        String message = e.getLocalizedMessage();
+        if (message == null)
+            message = e.toString();
+        ErrorMessage errorMessage = new ErrorMessage(new Date(), message, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	@ExceptionHandler(value = { Exception.class })
-	public ResponseEntity<ErrorMessage> handleAnyException(Exception e) {
-		String message = e.getLocalizedMessage();
-		if (message == null)
-			message = e.toString();
-		ErrorMessage errorMessage = new ErrorMessage(new Date(), message, HttpStatus.INTERNAL_SERVER_ERROR);
-		return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
-
-	@ExceptionHandler(MicroserviceException.class)
-	public ResponseEntity<ErrorResponse> serviceCallException(MicroserviceException e) {
-		return ResponseEntity.ok(e.getErrorResponse());
-	}
+    @ExceptionHandler(MicroserviceException.class)
+    public ResponseEntity<ErrorResponse> serviceCallException(MicroserviceException e) {
+        return new ResponseEntity<>(e.getErrorResponse(), HttpStatus.valueOf(e.getErrorResponse().getCode()));
+    }
 
 }
