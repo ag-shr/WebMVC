f657592410f140892b371a02c1fb6683acb16573
ag-shr <66527869+ag-shr@users.noreply.github.com>
2020-11-25 20:40:20 +0530

    Update ViewController.java

--- src/main/java/com/webapp/controllers/ViewController.java
+++ src/main/java/com/webapp/controllers/ViewController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
 @Controller
-@Controller
 public class ViewController {
 
     @GetMapping(path = "signUp")
